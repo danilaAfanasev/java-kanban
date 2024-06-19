a647bcf3449f372576b09c043023ec961b16c25a
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-06-19 11:31:51 +0300

    Изменен класс Main, добавлено создание задач и подзадач, изменение их и вывод

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        TaskManager taskManager = new TaskManager();
+
+        Task wipeDust = new Task("Протереть пыль", "С новой, чистой тряпкой");
+        Task wipeDustCreated = taskManager.addTask(wipeDust);
+        System.out.println(wipeDustCreated);
+
+        Task wipeDustUpdate = new Task(wipeDust.getId(), "Не забыть протереть пыль",
+                "Можно использовать старую тряпку, но лучше новую", Status.IN_PROGRESS);
+        Task wipeDustUpdated = taskManager.updateTask(wipeDustUpdate);
+        System.out.println(wipeDustUpdated);
+
+        Epic changeWallpaper = new Epic("Поменять обои", "Нужно успеть до Нового года");
+        taskManager.addEpic(changeWallpaper);
+        System.out.println(changeWallpaper);
+
+        Subtask changeWallpaperSubtaskFirst = new Subtask("Выбрать новые обои", "Минималистичные",
+                changeWallpaper.getId());
+        Subtask changeWallpaperSubtaskSecond = new Subtask("Поклеить новые обои", "Старые - выкинуть",
+                changeWallpaper.getId());
+        taskManager.addSubtask(changeWallpaperSubtaskFirst);
+        taskManager.addSubtask(changeWallpaperSubtaskSecond);
+        System.out.println(changeWallpaper);
+
+        changeWallpaperSubtaskFirst.setStatus(Status.DONE);
+        taskManager.updateSubtask(changeWallpaperSubtaskFirst);
+        System.out.println(changeWallpaper);
+
     }
 }
