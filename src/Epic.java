dd8902192ad0fbf4d1475899daa78d1311afbad3
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-06-19 11:28:55 +0300

    Добавлен класс Epic, отвечающий за большие задачи

--- src/Epic.java
+++ src/Epic.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Epic {
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private ArrayList<Subtask> subtasksList = new ArrayList<>();
+
+    public Epic (String name, String description){
+        super(name, description);
+    }
+
+    public Epic(int id, String name, String description, Status status){
+        super(id, name, description, status);
+    }
+
+    public void addSubtasks(Subtask subtask){
+        subtasksList.add(subtask);
+    }
+
+    public void clearSubstaks(){
+        subtasksList.clear();
+    }
+
+    public ArrayList<Subtask> getSubtasksList(){
+        return subtasksList;
+    }
+
+    public void setSubtasksList(ArrayList<Subtask> subtasksList){
+        this.subtasksList = subtasksList;
+    }
+
+    @Override
+    public String toString(){
+        return "Epic{" +
+                "name= " + getName() + '\'' +
+                ", description = " + getDescription() + '\'' +
+                ", id=" + getId() +
+                ", subtaskList.size = " + subtasksList.size() +
+                ", status = " + getStatus() +
+                '}';
+    }
 }
