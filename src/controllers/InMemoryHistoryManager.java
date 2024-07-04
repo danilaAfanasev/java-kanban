04bc7aff7bf7aad621303d8bbd497e203c3f6406
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-07-04 16:31:04 +0300

    Исправлена ошибка, из-за которой была уязвимость для изменений

--- src/controllers/InMemoryHistoryManager.java
+++ src/controllers/InMemoryHistoryManager.java
@@ -18,6 +18,6 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public List<Task> getHistory() {
-        return historyList;
+        return new ArrayList<>(historyList);
     }
 }
